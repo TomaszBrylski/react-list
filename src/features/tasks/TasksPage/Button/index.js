be6547570991169b13234b0d9bb9d9fdedf37a1b
Tomasz Brylski <brylskitomasz@gmail.com>
2024-02-24 00:33:13 +0100

    Added hidden styled to $:disabled

--- src/features/tasks/TasksPage/Button/index.js
+++ src/features/tasks/TasksPage/Button/index.js
@@ -1,26 +1,33 @@
 import styled from "styled-components";
 
-export default styled.button`
-    background: transparent;
-    color: ${({ theme }) => theme.color.teal};
-    border: none;
-    margin: 0 0 0 20px;
-    transition: filter 0.3s;
+const StyledButton = styled.button`
+  background: transparent;
+  color: ${({ theme }) => theme.color.teal};
+  border: none;
+  margin: 0 0 0 20px;
+  transition: filter 0.3s;
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        flex-basis: 100%;
-        margin: 10px;
-    }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    flex-basis: 100%;
+    margin: 10px;
+  }
 
-    &:hover {
-        filter: brightness(110%);
-    }
+  &:hover {
+    filter: brightness(110%);
+  }
 
-    &:active {
-        filter: brightness(120%);
-    }
+  &:active {
+    filter: brightness(120%);
+  }
 
-    &:disabled {
-    color: ${({ theme }) => theme.color.silver};
+  &:disabled,
+  &[hidden] { 
+    opacity: 0;
+    cursor: not-allowed;
+    pointer-events: none;
   }
 `;
+
+export default StyledButton;
+
+
