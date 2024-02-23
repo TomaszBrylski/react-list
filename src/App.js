bb7cd43f1cd548d5f0148abae8ec88ff29558bf5
Tomasz Brylski <brylskitomasz@gmail.com>
2024-02-23 23:04:37 +0100

    Formated code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import Tasks from "./features/tasks/TasksPage";
 import Task from "./features/tasks/TaskPage";
 import Author from "./features/author/Author";
-
-import { 
-  HashRouter, 
-  Switch, 
-  Route, 
-  Redirect 
-} from "react-router-dom";
+import Container from "./common/Container/styled.js";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import { toAuthor, toTask, toTasks } from "./routes";
 import Navigation from "./Navigation";
 
-export default () => (
-  <HashRouter>
-    <Navigation/>
-      
-      <Switch>
-        <Route path={toTask()}>
-          <Task />
-        </Route>
-        <Route path={toTasks()}>
-          <Tasks />
-        </Route>
-        <Route path={toAuthor()}>
-          <Author />
-        </Route>
-        <Route>
-          <Redirect to={toTasks()} />
-        </Route>
-      </Switch>
-  </HashRouter>
-);
+const App = () => {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Container>
+        <Switch>
+          <Route path={toTask()}>
+            <Task />
+          </Route>
+          <Route path={toTasks()}>
+            <Tasks />
+          </Route>
+          <Route path={toAuthor()}>
+            <Author />
+          </Route>
+          <Route path="/">
+            <Redirect to={toTasks()} />
+          </Route>
+        </Switch>
+      </Container>
+    </HashRouter>
+  );
+};
 
+export default App;
