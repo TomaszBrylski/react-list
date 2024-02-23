dd82420d6546970d3380ad5219e1934a42f628c5
Tomasz Brylski <brylskitomasz@gmail.com>
2024-02-23 23:16:46 +0100

    - Container attribute was removed from the code,  - removed {!!task &&} from body

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -1,27 +1,26 @@
 import Section from "../../../common/Section";
 import Header from "../../../common/Header";
-import { Container } from "../../../common/Container/styled";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
 
 function TaskPage() {
-    const { id } = useParams();
-    const task = useSelector((state) => getTaskById(state, id));
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
 
   return (
-      <Container>
-        <Header title="Task details" />
-        <Section
-          title={task ? task.content : "Task is not found"}
-          body={!!task && (
-              <>
-                <strong>Done:</strong>: {task.done ? "Yes" : "No"}
-              </>
-            )}
-        />
-      </Container>
-    );
-  }
+    <>
+      <Header title="Task details" />
+      <Section
+        title={task ? task.content : "Task is not found"}
+        body={
+          <>
+            <strong>Done:</strong> {task.done ? "Yes" : "No"}
+          </>
+        }
+      />
+    </>
+  );
+}
 
 export default TaskPage;
